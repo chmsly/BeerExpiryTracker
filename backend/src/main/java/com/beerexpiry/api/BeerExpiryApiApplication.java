611a49246fab8049668563baf2a2c0ed38d476ba
chmsly <chris.c.hemsley@gmail.com>
2025-05-23 16:03:05 -0600

    Add Spring Boot application entry point

---
+++ backend/src/main/java/com/beerexpiry/api/BeerExpiryApiApplication.java
@@ -0,0 +1,11 @@
+package com.beerexpiry.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerExpiryApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeerExpiryApiApplication.class, args);
+    }
+} 
